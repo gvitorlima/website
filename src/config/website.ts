5ec40ab018353c735792d47f0d343c275cf799c5
gabriel vitor <gabriel.lima@dantaseletro.com.br>
2024-05-02 14:06:22 -0300

    first commit

---
+++ src/config/website.ts
@@ -0,0 +1,6 @@
+export const WEBSITE_CONFIG = {
+  title: "Gabriel Vitor Web Dev",
+  description: "Desenvolvedor Web Full Stack | PHP | Laravel | ReactJS",
+  ogImage: "og.png",
+  url: "gabrielvitor.com"
+}
