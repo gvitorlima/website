0f22a4e1d8f1e4445df65048c5afdc60a802dcab
gvitorlima <git624886@gmail.com>
2024-05-03 13:45:21 -0300

    feat: contact links

--- src/config/website.ts
+++ src/config/website.ts
@@ -1,6 +1,12 @@
 export const WEBSITE_CONFIG = {
   title: "Gabriel Vitor Web Dev",
   description: "Desenvolvedor Web Full Stack | PHP | Laravel | ReactJS",
+  contact: {
+    linkedIn:
+      "https://br.linkedin.com/in/gabriel-vitor-2aa1b9229?trk=people-guest_people_search-card",
+    github: "https://github.com/gvitorlima",
+    email: "gabrielvitorlima.dev@gmail.com"
+  },
   ogImage: "og.png",
-  url: "gabrielvitor.com"
-}
+  url: "gabriel.example.com",
+};
